afdc6254090b886ca5fecddd523c2350dd684b35
SemihTut <yalaantv@gmail.com>
2021-03-06 22:27:37 +0200

    asd

--- src/test/java/EvilTester/StepDefinitions/Hooks.java
+++ src/test/java/EvilTester/StepDefinitions/Hooks.java
@@ -1,14 +1,8 @@
 package EvilTester.StepDefinitions;
 
-import EvilTester.Utilities.ConfigurationReader;
-import EvilTester.Utilities.Driver;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class Hooks {
